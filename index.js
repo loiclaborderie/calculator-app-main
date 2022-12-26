66315233c508b310e9686f743c623279ac304929
Loic L <l.laborderie-boulou@tbs-education.org>
2022-12-26 19:35:44 +0100

    added simple input.value functionality to btns

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const range = document.getElementById("theme-range");
 const body = document.querySelector("body");
+const input = document.querySelector(".result");
+const reset = document.querySelector(".btn-reset");
+const del = document.querySelector(".btn-del");
+const zero = document.querySelector(".btn-zero");
+const one = document.querySelector(".btn-un");
+const two = document.querySelector(".btn-deux");
+const three = document.querySelector(".btn-trois");
+const four = document.querySelector(".btn-quatre");
+const five = document.querySelector(".btn-cinq");
+const six = document.querySelector(".btn-six");
+const seven = document.querySelector(".btn-sept");
+const eight = document.querySelector(".btn-huit");
+const nine = document.querySelector(".btn-neuf");
+const point = document.querySelector(".btn-point");
+const divide = document.querySelector(".btn-divise");
+const times = document.querySelector(".btn-fois");
+const plus = document.querySelector(".btn-plus");
+const minus = document.querySelector(".btn-moins");
+const equal = document.querySelector(".btn-egal");
+
 body.className = "theme-1";
 range.addEventListener("input", () => {
   const value = range.value;
@@ -12,4 +32,56 @@ range.addEventListener("input", () => {
     body.className = "theme-3";
   }
 });
-console.log(range.value);
+input.value = 369;
+
+reset.addEventListener("click", () => {
+  input.value = "";
+});
+del.addEventListener("click", () => {
+  input.value = input.value.substring(0, input.value.length - 1);
+});
+zero.addEventListener("click", () => {
+  input.value += 0;
+});
+one.addEventListener("click", () => {
+  input.value += 1;
+});
+two.addEventListener("click", () => {
+  input.value += 2;
+});
+three.addEventListener("click", () => {
+  input.value += 3;
+});
+four.addEventListener("click", () => {
+  input.value += 4;
+});
+five.addEventListener("click", () => {
+  input.value += 5;
+});
+six.addEventListener("click", () => {
+  input.value += 6;
+});
+seven.addEventListener("click", () => {
+  input.value += 7;
+});
+eight.addEventListener("click", () => {
+  input.value += 8;
+});
+nine.addEventListener("click", () => {
+  input.value += 9;
+});
+point.addEventListener("click", () => {
+  input.value += ".";
+});
+times.addEventListener("click", () => {
+  input.value += "*";
+});
+divide.addEventListener("click", () => {
+  input.value += "/";
+});
+plus.addEventListener("click", () => {
+  input.value += "+";
+});
+minus.addEventListener("click", () => {
+  input.value += "-";
+});
